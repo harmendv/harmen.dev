6b36d64261bb49302a3ac1864cb1d2b5dc40de9a
Harmen de Vries <harmen@made.frl>
2025-08-18 16:14:43 +0200

    Finish

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,14 @@ import { GithubIcon } from 'lucide-vue-next';
 </script>
 
 <template>
-    <div class="flex flex-col items-center">
-        <img src="/images/logo.png" class="w-20 mb-3" alt="harmendv">
-        <a href="https://github.com/harmendv" class="text-sm underline inline-flex items-center gap-1"><GithubIcon :size="16"></GithubIcon>harmendv</a>
-    </div>
+    <a href="https://github.com/harmendv" class="flex flex-col items-center hover-effect cursor-help" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+        <img src="/images/logo.png" class="w-24 mb-3" alt="harmendv">
+    </a>
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-
 html {
     height: 100%;
-    font-family: 'Inter', sans-serif;
 }
 
 body {
@@ -32,5 +28,4 @@ body {
     align-items: center;
     justify-content: center;
 }
-
 </style>
