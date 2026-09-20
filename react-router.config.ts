bc323249c4ae0df1e9eeb808fa6ce042b1471ae5
harmendv <harmendv@gmail.com>
2026-09-20 15:33:39 +0200

    Add site.webmanifest and enable prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: false,
-  prerender: true,
+  async prerender() {
+    return ["/", "/privacy", "/terms"];
+  },
 } satisfies Config;
